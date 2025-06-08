ac77096842f6b14cbeb484494302ecd4ca4c5673
premiumhomecy <soykokpremiumhomescyprus@gmail.com>
2025-06-08 13:18:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-
+your-repo-name/premium2
+├── .streamlit/
+│   └── config.toml  (Optional: for Streamlit theme/settings)
+├── app.py
+├── requirements.txt
+├── Arial.ttf        (Optional, for consistent PDF fonts)
+└── Arial Bold.ttf   (Optional, for consistent PDF fonts)
